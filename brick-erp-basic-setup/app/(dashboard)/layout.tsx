0a410645bbcae336871490e2ae268359e9e00234
terichuco855-cell <terichuco855@gmail.com>
2026-05-15 19:40:39 -0400

    Add key prop to Sidebar component

--- brick-erp-basic-setup/app/(dashboard)/layout.tsx
+++ brick-erp-basic-setup/app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="flex h-screen overflow-hidden bg-background">
-      <Sidebar />
+      <Sidebar key="sidebar-instance" />
       <div className="flex flex-1 flex-col overflow-hidden">
         <main className="flex-1 overflow-y-auto">{children}</main>
       </div>
